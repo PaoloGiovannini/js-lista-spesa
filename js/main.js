765a67c0529cc3c974ac138b278a2b95a2779a3e
Paolo Giovannini <giovannini13@gmail.com>
2023-03-01 16:29:18 +0100

    aggiunti punto e virgola

--- js/main.js
+++ js/main.js
@@ -4,6 +4,6 @@ const listaDom = document.querySelector('.lista');
 
 let i = 0;
 while (i < listaOggettiSpesa.length){
-    listaDom.innerHTML += `<li>${listaOggettiSpesa[i]}</li>`
-    i++
+    listaDom.innerHTML += `<li>${listaOggettiSpesa[i]}</li>`;
+    i++ ;
 }
